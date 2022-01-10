0c9ca18678bd57d5d8b8f281a2a59eaf2572c142
Davic0444 <34513322+Davic0444@users.noreply.github.com>
2022-01-10 06:44:32 +0100

    REST method for search by id

--- src/main/java/hu/david/ecommerce/dao/ProductRepository.java
+++ src/main/java/hu/david/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,14 @@
 package hu.david.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hu.david.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
