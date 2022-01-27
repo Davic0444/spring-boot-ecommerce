94084556786cbdf1e049c9abc42853a7663f7098
Davic0444 <34513322+Davic0444@users.noreply.github.com>
2022-01-27 16:11:09 +0100

    Solved CORS issue

--- src/main/java/hu/david/ecommerce/dao/CountryRepository.java
+++ src/main/java/hu/david/ecommerce/dao/CountryRepository.java
@@ -2,6 +2,7 @@ package hu.david.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import hu.david.ecommerce.entity.Country;
 
